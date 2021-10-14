2b69977a5e9160a4c0434c7695bd186a92f54905
Ivan F Gushchin @ DellCrow 18 <konara@ya.ru>
2021-10-15 01:55:03 +0400

    inside docker building done as root user. FORCE_UNSAFE is required

--- device/build_rv1109-ai-uvc.sh
+++ device/build_rv1109-ai-uvc.sh
@@ -2,7 +2,7 @@
 
 .repo/repo/repo sync -m rv1126_rv1109_linux_ai_camera_20210904.xml -c
 ./build.sh cam-crv1109s2u-uvcc.mk
-./build.sh
+FORCE_UNSAFE_CONFIGURE=1 ./build.sh
 
 
 cp rockdev/{parameter.txt,recovery.img,rootfs.ext4,rootfs.img,uboot.img,userdata.img,MiniLoaderAll.bin,boot.img,misc.img,oem.img} /dist
