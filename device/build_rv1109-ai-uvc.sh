70b8cec90e3ffc01e8b8111c4fe42cb67b733b54
Ivan F Gushchin @ DellCrow 18 <konara@ya.ru>
2021-10-18 01:58:59 +0400

    all 4 targets are build in a 2 set

--- device/build_rv1109-ai-uvc.sh
+++ device/build_rv1109-ai-uvc.sh
@@ -5,5 +5,24 @@
 FORCE_UNSAFE_CONFIGURE=1 ./build.sh
 
 
-cp rockdev/{parameter.txt,recovery.img,rootfs.ext4,rootfs.img,uboot.img,userdata.img,MiniLoaderAll.bin,boot.img,misc.img,oem.img} /dist
-cp rockdev/pack/*.img /dist/
+cp rockdev/{parameter.txt,recovery.img,rootfs.ext4,rootfs.img,uboot.img,userdata.img,MiniLoaderAll.bin,boot.img,misc.img,oem.img} /dist/rv1109-ai-uvc
+cp rockdev/pack/*.img /dist/rv1109-ai-uvc
+
+./repo/repo/repo sync -m rv1126_rv1109_linux_20210904_facial_gate.xml -c
+./build.sh cam-crv1109s2u-facial_gate.mk
+FORCE_UNSAFE_CONFIGURE=1 ./build.sh
+cp rockdev/{parameter.txt,recovery.img,rootfs.ext4,rootfs.img,uboot.img,userdata.img,MiniLoaderAll.bin,boot.img,misc.img,oem.img} /dist/rv1109-facial-gate
+cp rockdev/pack/*.img /dist/rv1109-facial-gate
+
+
+.repo/repo/repo sync -m rv1126_rv1109_linux_ai_camera_20210904.xml -c
+./build.sh cam-crv1126s2u-uvcc.mk
+FORCE_UNSAFE_CONFIGURE=1 ./build.sh
+cp rockdev/{parameter.txt,recovery.img,rootfs.ext4,rootfs.img,uboot.img,userdata.img,MiniLoaderAll.bin,boot.img,misc.img,oem.img} /dist/rv1126-ai-uvc
+cp rockdev/pack/*.img /dist/rv1126-ai-uvc
+
+.repo/repo/repo sync -m rv1126_rv1109_linux_20210904_facial_gate.xml -c
+./build.sh cam-crv1126s2u-facial_gate.mk
+FORCE_UNSAFE_CONFIGURE=1 ./build.sh
+cp rockdev/{parameter.txt,recovery.img,rootfs.ext4,rootfs.img,uboot.img,userdata.img,MiniLoaderAll.bin,boot.img,misc.img,oem.img} /dist/rv1126-facial-gate
+cp rockdev/pack/*.img /dist/rv1126-facial-gate
