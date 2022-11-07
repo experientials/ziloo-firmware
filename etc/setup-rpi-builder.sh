fa99bfc53957197648f746db86262121bb62f425
Henrik Vendelbo <henrik@thepia.com>
2022-11-07 11:07:09 +0100

    Fixed rpi install script

--- etc/setup-rpi-builder.sh
+++ etc/setup-rpi-builder.sh
@@ -5,6 +5,7 @@ sudo systemctl start ssh
 echo SSH ip address
 hostname -i
 
+sudo apt update
 sudo apt install gparted etckeeper minicom code
 
 sudo locale-gen en_GB.UTF-8 UTF-8
@@ -16,16 +17,16 @@ sudo apt install tex-common
 sudo dpkg --configure tex-common
 
 # Docs generation support
-sudo apt install xmlto help2man groff libreoffice-writer vim bash-completion fop dblatex texi2html docbook-utils asciidoc xmlto help2man groff  
+sudo apt install -y xmlto help2man groff libreoffice-writer vim bash-completion fop dblatex texi2html docbook-utils asciidoc groff  
 
 # Networking support
-sudo apt install dnsmasq u-boot-tools tftpd-hpa tftp sshpass ssh-askpass socat parprouted dhcp-helper nfs-kernel-server rpcbind
+sudo apt install -y dnsmasq u-boot-tools tftpd-hpa tftp sshpass ssh-askpass socat parprouted dhcp-helper nfs-kernel-server rpcbind
 
 # Disk Image building
-sudo apt install mtools parted binfmt-support debootstrap dosfstools fdisk gdisk kpartx xxd squashfs-tools u-boot-tools genext2fs xz-utils lz4 liblz4-tool 
+sudo apt install -y mtools parted binfmt-support debootstrap dosfstools fdisk gdisk kpartx xxd squashfs-tools u-boot-tools genext2fs xz-utils lz4 liblz4-tool 
 
 # Compiling
-sudo apt install autoconf libtool automake diffstat libudev-dev libusb-1.0-0-dev gawk \
+sudo apt install -y autoconf libtool automake diffstat libudev-dev libusb-1.0-0-dev gawk \
     cmake intltool pkg-config patch patchutils m4 bison flex tcl lzop texinfo xterm
 
 # Git user
@@ -43,9 +44,9 @@ git reset --hard
 
 # etckeeper
 sudo etckeeper init /etc
-sudo git branch -m hw/builder
+sudo git branch -m builder/etc
 sudo git remote add origin git@github.com:experientials/ziloo-firmware.git
-sudo git push --set-upstream origin hw/builder
+sudo git push --set-upstream origin builder/etc
 cd /etc
 sudo git status
 
